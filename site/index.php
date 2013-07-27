797ac561df375eea0240735c1b1d53cf43dd05d6
Arthur Vieira Moreira Lucena <arthurvmx5@gmail.com>
2013-07-27 17:22:11 -0300

    Full HTML5 translation

--- site/index.php
+++ site/index.php
@@ -1,14 +1,16 @@
+﻿<!-- http://coursesweb.net/html/html5-quick-tutorial_t -->
 <!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<!--[if lt IE 7]>      <html lang=”pt-br” class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html lang=”pt-br” class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html lang=”pt-br” class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html lang=”pt-br” class="no-js"> <!--<![endif]-->
+<html>
 <head>
 	<meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	<meta name="description" content="">
     <meta name="viewport" content="width=device-width">
-	<meta name="author" content="Arthur Vieira Moreira Lucena">
+	<meta name="author" content="Arthur VML - @art_vieira">
 	<title>Layout</title>
 	
 	<link rel="stylesheet" type="text/css" href="/css/normalize.min.css">
@@ -16,89 +18,111 @@
 	<link rel="stylesheet" type="text/css" href="/css/main.css">
 	
 	<script type="text/javascript" src="/js/vendor/modernizr-2.6.2.min.js"></script>
+	<style>
+		header, hgroup, nav, section, article, aside, footer {
+			
+		}
+
+		#wrapper {
+			width:800px; 
+			margin:0 auto;
+		}
+
+		#wrapper section#option {
+			float:left;
+			width:268px;
+			height:480px;
+		}
+
+		#wrapper section#page {
+			float:right;
+			width:522px;
+			min-height:480px;
+			background-color:#eee;
+			color:#666;  
+		}
+
+		#wrapper footer#page_footer {
+			clear:both;
+			width:100%;
+			height:40px;
+			border-top:#ED2525 solid 2px;
+			font-size:1.7em;
+			text-shadow: 1px 0px #666;
+		}
+	</style>
+
 </head>
 <body>
 <!--[if lt IE 7]>
-	<p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+<p class="chromeframe">Seu navegador está desatualizado! Por favor atualize <a href="http://browsehappy.com/">AQUI</a> seu navegador ou <a href="http://www.google.com/chromeframe/?redirect=true">ative Google Chrome Frame</a> para melhorar sua experiencia de navegação neste site.<p>
 <![endif]-->
+    
 <div id="wrapper">
-	<div style="width:800px; margin:0 auto;">
-        <div style="width:268px;float:left;">
-			<div id="nav" style="height:286px;">
-				<a href="/home/">
-					<div id="xm-bt-home" class="xm-bt">
-					    <img class="xm-bt-img" src="/images/home.png" /> 
-					    <div class="xm-bt-sub">arthur</div>
-					</div>
-				</a>
-				
-				<a href="/contact/">
-					<div id="xm-bt-contact" class="xm-bt">
-					    <img class="xm-bt-img" src="/images/contact.png" />    
-					    <div class="xm-bt-sub">contact</div>
-					</div>
-				</a>
-				
-				<a href="/work/">
-					<div id="xm-bt-work" class="xm-bt">
-					    <img class="xm-bt-img" src="/images/work.png" /> 
-					    <div class="xm-bt-sub">work</div>
-					</div>
-				</a>
-				
-				<a href="/bio/">
-					<div id="xm-bt-bio" class="xm-bt">
-					    <img class="xm-bt-img" src="/images/bio.png" />    
-					    <div class="xm-bt-sub">bio</div>
-					</div>
-				</a>
+<section id="option">
+	<nav>
+		<a href="/home/">
+			<div id="xm-bt-home" class="xm-bt">
+			    <img class="xm-bt-img" src="/images/home.png" /> 
+			    <div class="xm-bt-sub">arthur</div>
 			</div>
-			<div id="logo" style="height:196px; width:268px; text-align:center;">
-				<span id="bt-es" class="multi-lang" style="margin-top:10px; margin-left:5px; margin-right:9px; background-color:#666; opacity:0.2; color:#ddd; font-size:1.7em; display:block;">Spanish</span>
-				<span id="bt-ptbr" class="multi-lang" style="margin-top:10px; margin-left:5px; margin-right:9px; background-color:#666; opacity:0.2; color:#ddd; font-size:1.7em; display:block;">Portuguese</span>
-				<span id="bt-en" class="multi-lang" style="margin-top:10px; margin-left:5px; margin-right:9px; background-color:#666; opacity:0.2; color:#ddd; font-size:1.7em; display:block;">English</span>
-				<!-- <img src="http://arthurvieira.com/images/mlogo.png" style="height:180px; width:180px; margin-top:10px;" />  -->
+		</a>
+		
+		<a href="/contact/">
+			<div id="xm-bt-contact" class="xm-bt">
+			    <img class="xm-bt-img" src="/images/contact.png" />    
+			    <div class="xm-bt-sub">contact</div>
 			</div>
-        </div>
-        <div id="cont" style="min-height:494px; width:532px; float:left; ">
-        	<div class="background-translucent"></div>
-			<?php 
-				if (isset($_GET['page'])) {
-					include 'pages/'.$_GET['page'].'.php';
-				} else {
-					include 'pages/home.php';
-				}
-			?>
-        </div>
-		<div id="social" style="height:40px; width:100%; float:left;">
-			<div class="background-translucent"></div>
-        	<div class="text-opaque">
-        		<div class="social-bt" style="float:left;"> 
-        			<img src="/images/social/deviantart.png" />    
-        		</div>
-        		<div class="social-bt" style="float:left;"> 
-        			<img src="/images/social/facebook.png" />    
-        		</div>
-        		<div class="social-bt" style="float:left;"> 
-        			<img src="/images/social/github.png" />    
-        		</div>
-        		<div class="social-bt" style="float:left;"> 
-        			<img src="/images/social/linkedin.png" />    
-        		</div>
-        		<div class="social-bt" style="float:left;"> 
-        			<img src="/images/social/skype.png" />    
-        		</div>
-        		<div class="social-bt" style="float:left;"> 
-        			<img src="/images/social/twitter.png" />    
-        		</div>
-        		<div style="float:right; margin-top:8px; margin-right:5px; color:black;">
-        			arthur vieira m. lucena, 2013
-        		</div>
-        	</div>
-		</div>
-    </div>
-</div>
+		</a>
+		
+		<a href="/work/">
+			<div id="xm-bt-work" class="xm-bt">
+			    <img class="xm-bt-img" src="/images/work.png" /> 
+			    <div class="xm-bt-sub">work</div>
+			</div>
+		</a>
+		
+		<a href="/bio/">
+			<div id="xm-bt-bio" class="xm-bt">
+			    <img class="xm-bt-img" src="/images/bio.png" />    
+			    <div class="xm-bt-sub">bio</div>
+			</div>
+		</a>
+	</nav>
+	
+	<aside style="clear:both; margin-left: 5px;">
+		<span id="bt-es" class="multi-lang"><img src="/images/lang/es.png" height="28px" width="46" /></span>
+		<span id="bt-ptbr" class="multi-lang"><img src="/images/lang/ptbr.png" height="28px" width="46" /></span>
+		<span id="bt-en" class="multi-lang"><img src="/images/lang/en.png" height="28px" width="46" /></span>
+	</aside>
+</section>
 
+<section id="page">
+    <article>
+        <?php 
+			if (isset($_GET['page'])) {
+				include 'pages/'.$_GET['page'].'.php';
+			} else {
+				include 'pages/home.php';
+			}
+		?>
+    </article>        
+</section>
+    
+<footer id="page_footer">
+	<img class="social-bt" src="/images/social/deviantart.png" />    
+	<img class="social-bt" src="/images/social/facebook.png" />    
+	<img class="social-bt" src="/images/social/github.png" />    
+	<img class="social-bt" src="/images/social/linkedin.png" />    
+	<img class="social-bt" src="/images/social/skype.png" />    
+	<img class="social-bt" src="/images/social/twitter.png" />    
+	
+	<span style="float:right; margin-top:8px; margin-right:5px; color:black;">
+		arthur vieira m. lucena, 2013
+	</span>
+</footer>
+</div>
+    
 <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 <script>window.jQuery || document.write('<script type="text/javascript" src="/js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
 <script type="text/javascript" src="/js/plugins.js"></script>
@@ -109,24 +133,11 @@
   _gaq.push(['_trackPageview']);
 
   (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
   })();
 </script>
 
 </body>
 </html>
-<!-- 
-Layout bacana
-http://programming.mvergel.com/2011/08/speed-up-eclipse.html#.UNspGOR318E
-
-inspiração:
-http://www.amazeelabs.com/en
-http://luciasoto.es/
-http://lab.4muladesign.com/dribbble/
-http://www.4muladesign.com/
-
-ler:
-http://tableless.com.br/mobile-first-a-arte-de-pensar-com-foco/
--->

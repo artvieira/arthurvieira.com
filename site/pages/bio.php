8e0fd1207b641683ff79c43476d788337df3f65d
Arthur Vieira Moreira Lucena <arthurvmx5@gmail.com>
2013-07-27 17:19:42 -0300

    Update bio partial

--- site/pages/bio.php
+++ site/pages/bio.php
@@ -2,7 +2,7 @@
 	<h1>Who I Am</h1>
     <p>I am Arthur, programmer, illustrator, designer, curious, eternal student and racing fan.</p>
     
-    <p>Making things beautiful and fast is a philosophy major car companies like Lamborghini, Audi, Ferrari and etc, but I believe it can be applied in all aspects of life. I always try to apply it in my life and the things I do.</p>
+    <p>Making things beautiful and fast is a philosophy major car/motorcycles companies like Lamborghini, Audi, Porsche, Pagani, Ducati and etc, but I believe it can be applied in all aspects of life. I always try to apply it in my life and the things I do.</p>
     
     <p>Since 2009 I did various freelance work in various segments of computing. In 2011 I started to work hard as a Java developer.</p>
     
@@ -21,13 +21,16 @@
         <li>Webserver configuration</li>
     </ul>
     
-    <h2>I do it</h2>
+    <h2>With what I do</h2>
     <ul style="font-size: 2em">
-        <li>Creative Suite</li>
+        <li>Adobe Creative Suite
         <li>Aptana Studio</li>
         <li>Xampp</li>
-        <li>HTML5/CSS3/JS/jQuery/PHP5</li>
-        <li>Developer machine PC Windows</li>
+        <li>HTML5</li>
+        <li>CSS3</li>
+        <li>JS</li>
+        <li>jQuery</li>
+        <li>PHP5</li>
         <li>Server CentOS 6.3</li>
     </ul>
 </div>

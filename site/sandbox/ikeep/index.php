c9ee65a5eb75242fefefd98b6a249aa6f05982e5
Arthur Vieira Moreira Lucena <arthurvmx5@gmail.com>
2013-08-05 14:15:28 -0300

    [ikeep]

--- site/sandbox/ikeep/index.php
+++ site/sandbox/ikeep/index.php
@@ -12,33 +12,38 @@
 	    #wrapper {
 	       width:850px; 
            margin:0 auto;
+		   margin-top:25px;
 	    }
 	    
 	    body {
-	        background-color:#EEE;
+	        background-color:#eee;
 	        font-size:62.5%;
 	    }
 	
-	    body, input[type=text], input[type=submit], input[type=reset] {
+	    body, input {
 	        font-family:'Bariol-Regular';
             color:#666;
-            outline: none;
+			outline: none;
             font-smoothing:antialiased;
             -moz-font-smoothing:antialiased;
             -webkit-font-smoothing:antialiased;
             -o-font-smoothing:antialiased;
   	    }
-  	    
-  	    input[type=text] {
-  	        width: 260;
-  	    }
-	    
-	    input[type=text], #submit, #reset, #content {
-	        font-size: 1.2em;
-	    }
+		
+		h1, h2, h3, h4, h5 { 
+			padding: 0px;
+			margin: 0px;
+		}
+		
+		span {
+			font-size: 1.4em;
+		}
 	    
 	    #new-note {
-	    	width: 800px;
+			font-size: 1.8em;
+	    	width: 828px;
+			margin:10px;
+			padding:2px;
 	    }
 	    
 	    #new-note-div {
@@ -48,15 +53,37 @@
 	    }
 	    
 	    #notes {
-	    	margin-top:10px;
-	    	
+	    	margin-top:10px; 
 	    }
 	    
 	    .note {
-	    	border:1px solid #999;
+	    	border:1px solid #bbb;
 	    	width:180px;
-	    	padding:10px;
+			height:240px;
+			float:left;
+			margin:5px;
 	    }
+
+		.note-color {
+			width:100%;
+			height:3px;
+			background-color:#f00;
+		}
+		
+		.note-text {
+			width:160;
+			height:177;
+			padding:10px;
+			background-color:#fff;
+		}
+		
+		.note-time {
+			font-size: 1.2em;
+			width:160;
+			height:40px;
+			padding:10px;
+			text-align:right;
+		}
 	</style>
 </head>
 
@@ -65,15 +92,110 @@
 	<section id="new-note-div">
 		<input id="new-note" type="text" value="new note" />
 	</section>
+	
 	<section id="notes">
 		<section class="note">
-			<h1>Titulo da nota</h1>
-			<label>Conteudo da nota bla bla bla</label>
+			<section class="note-color"></section>
+			<section class="note-text">
+				<h1>Titulo da nota</h1>
+				<span style="text-align:justify">Conteudo da gd gdsgfd  bla bla bla Conteudo da nota bla bla bla Conteudo da nota bla bla bla</span>
+			</section>
+			<section class="note-time">7:22AM</section>
 		</section>
-	</section>
+		
+		<section class="note">
+			<section class="note-color"></section>
+			<section class="note-text">
+				<h1>Titulo da nota</h1>
+				<span style="text-align:justify">Conteudo da gd gdsgfd  bla bla bla Conteudo da nota bla bla bla Conteudo da nota bla bla bla</span>
+			</section>
+			<section class="note-time">7:22AM</section>
+		</section>
+		
+		<section class="note">
+			<section class="note-color"></section>
+			<section class="note-text">
+				<h1>Titulo da nota</h1>
+				<span style="text-align:justify">Conteudo da gd gdsgfd  bla bla bla Conteudo da nota bla bla bla Conteudo da nota bla bla bla</span>
+			</section>
+			<section class="note-time">7:22AM</section>
+		</section>
+		
+		
+		<section class="note">
+			<section class="note-color"></section>
+			<section class="note-text">
+				<h1>Titulo da nota</h1>
+				<span style="text-align:justify">Conteudo da gd gdsgfd  bla bla bla Conteudo da nota bla bla bla Conteudo da nota bla bla bla</span>
+			</section>
+			<section class="note-time">7:22AM</section>
+		</section>
+		
+		<section class="note">
+			<section class="note-color"></section>
+			<section class="note-text">
+				<h1>Titulo da nota</h1>
+				<span style="text-align:justify">Conteudo da gd gdsgfd  bla bla bla Conteudo da nota bla bla bla Conteudo da nota bla bla bla</span>
+			</section>
+			<section class="note-time">7:22AM</section>
+		</section>
+		
+		<section class="note">
+			<section class="note-color"></section>
+			<section class="note-text">
+				<h1>Titulo da nota</h1>
+				<span style="text-align:justify">Conteudo da gd gdsgfd  bla bla bla Conteudo da nota bla bla bla Conteudo da nota bla bla bla</span>
+			</section>
+			<section class="note-time">7:22AM</section>
+		</section>
+		
+		<section class="note">
+			<section class="note-color"></section>
+			<section class="note-text">
+				<h1>Titulo da nota</h1>
+				<span style="text-align:justify">Conteudo da gd gdsgfd  bla bla bla Conteudo da nota bla bla bla Conteudo da nota bla bla bla</span>
+			</section>
+			<section class="note-time">7:22AM</section>
+		</section>
+		
+		<section class="note">
+			<section class="note-color"></section>
+			<section class="note-text">
+				<h1>Titulo da nota</h1>
+				<span style="text-align:justify">Conteudo da gd gdsgfd  bla bla bla Conteudo da nota bla bla bla Conteudo da nota bla bla bla</span>
+			</section>
+			<section class="note-time">7:22AM</section>
+		</section>
+		
+		<section class="note">
+			<section class="note-color"></section>
+			<section class="note-text">
+				<h1>Titulo da nota</h1>
+				<span style="text-align:justify">Conteudo da gd gdsgfd  bla bla bla Conteudo da nota bla bla bla Conteudo da nota bla bla bla</span>
+			</section>
+			<section class="note-time">7:22AM</section>
+		</section>
+		
+		<section class="note">
+			<section class="note-color"></section>
+			<section class="note-text">
+				<h1>Titulo da nota</h1>
+				<span style="text-align:justify">Conteudo da gd gdsgfd  bla bla bla Conteudo da nota bla bla bla Conteudo da nota bla bla bla</span>
+			</section>
+			<section class="note-time">7:22AM</section>
+		</section>
+		
+	</section
 	
-    
 </section>
+
+<script type="text/javascript" src="http://code.jquery.com/jquery-2.0.3.js"></script>
+<script
+window.scrollTo(0, 1);
+
+(function($) {
+
+})(jQuery);
+</script>
 </body>
 </html>
-
